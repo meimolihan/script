0bb0b1ff46c618a6b3fee4f0e71eeabd01607522
meimolihan <meimolihan@gmail.com>
2025-05-12 03:06:48 +0800

    自动提交于 2025/05/12 周一  3:06:46.48

--- sh/compose/git_clone_docker.sh
+++ sh/compose/git_clone_docker.sh
@@ -4,26 +4,45 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# 定义仓库地址，每行一个
+# 定义仓库地址和中文注释，索引保持一致
 repo[1]="git@gitee.com:meimolihan/1panel.git"
+comment[1]="服务器管理"
 repo[2]="git@gitee.com:meimolihan/aipan.git"
+comment[2]="爱盼影视"
 repo[3]="git@gitee.com:meimolihan/alist.git"
+comment[3]="云盘目录程序"
 repo[4]="git@gitee.com:meimolihan/docker-manage.git"
+comment[4]="Docker管理工具"
 repo[5]="git@gitee.com:meimolihan/emby.git"
+comment[5]="媒体服务器"
 repo[6]="git@gitee.com:meimolihan/halo.git"
+comment[6]="博客系统"
 repo[7]="git@gitee.com:meimolihan/istoreos.git"
+comment[7]="路由器系统"
 repo[8]="git@gitee.com:meimolihan/it-tools.git"
+comment[8]="IT工具箱"
 repo[9]="git@gitee.com:meimolihan/kspeeder.git"
+comment[9]="网络加速器"
 repo[10]="git@gitee.com:meimolihan/nastools.git"
+comment[10]="影视刮削"
 repo[11]="git@gitee.com:meimolihan/random-pic-api.git"
+comment[11]="随机图片API"
 repo[12]="git@gitee.com:meimolihan/sun-panel.git"
+comment[12]="导航面板"
 repo[13]="git@gitee.com:meimolihan/tvhelper.git"
+comment[13]="电视助手"
 repo[14]="git@gitee.com:meimolihan/uptime-kuma.git"
+comment[14]="网站监控器"
 repo[15]="git@gitee.com:meimolihan/xiaomusic.git"
+comment[15]="小米音乐"
 repo[16]="git@gitee.com:meimolihan/xunlei.git"
+comment[16]="迅雷下载"
 repo[17]="git@gitee.com:meimolihan/md.git"
+comment[17]="Markdown工具"
 repo[18]="git@gitee.com:meimolihan/easyvoice.git"
+comment[18]="语音转文字"
 repo[19]="git@gitee.com:meimolihan/dpanel.git"
+comment[19]="Docker面板"
 
 menu() {
     clear
@@ -33,7 +52,7 @@ menu() {
     for index in "${!repo[@]}"; do
         repo_url="${repo[$index]}"
         repo_name=$(basename "$repo_url" .git)
-        echo "$index. 克隆仓库：$repo_name"
+        echo "$index. ${comment[$index]}--$repo_name"
     done
     echo "========================"
     echo "a. 自定义克隆仓库地址"
@@ -59,13 +78,13 @@ menu() {
         for index in "${!repo[@]}"; do
             repo_url="${repo[$index]}"
             repo_name=$(basename "$repo_url" .git)
-            echo "正在克隆仓库：$repo_name"
+            echo "正在克隆仓库：${comment[$index]}--$repo_name"
             git clone "${repo[$index]}"
             if [ $? -ne 0 ]; then
-                echo "[错误] 克隆仓库：$repo_name 失败"
+                echo "[错误] 克隆仓库：${comment[$index]}--$repo_name 失败"
                 all_success=0
             else
-                echo "[成功] 克隆仓库：$repo_name 完成"
+                echo "[成功] 克隆仓库：${comment[$index]}--$repo_name 完成"
             fi
             echo
         done
@@ -82,13 +101,14 @@ menu() {
 
     if [ "$lc_choice" = "a" ]; then
         read -p "请输入自定义仓库克隆地址: " custom_repo
+        read -p "请输入仓库中文注释: " custom_comment
         repo_name=$(basename "$custom_repo" .git)
-        echo "正在克隆仓库：$repo_name"
+        echo "正在克隆仓库：${custom_comment}--$repo_name"
         git clone "$custom_repo"
         if [ $? -eq 0 ]; then
-            echo "[成功] 克隆仓库：$repo_name 完成"
+            echo "[成功] 克隆仓库：${custom_comment}--$repo_name 完成"
         else
-            echo "[错误] 克隆仓库：$repo_name 失败"
+            echo "[错误] 克隆仓库：${custom_comment}--$repo_name 失败"
         fi
         read -p "按回车键继续..."
         menu
@@ -99,12 +119,12 @@ menu() {
         if [ -n "${repo[$choice]}" ]; then
             repo_url="${repo[$choice]}"
             repo_name=$(basename "$repo_url" .git)
-            echo "正在克隆仓库：$repo_name"
+            echo "正在克隆仓库：${comment[$choice]}--$repo_name"
             git clone "${repo[$choice]}"
             if [ $? -eq 0 ]; then
-                echo "[成功] 克隆仓库：$repo_name 完成"
+                echo "[成功] 克隆仓库：${comment[$choice]}--$repo_name 完成"
             else
-                echo "[错误] 克隆仓库：$repo_name 失败"
+                echo "[错误] 克隆仓库：${comment[$choice]}--$repo_name 失败"
             fi
             read -p "按回车键继续..."
             menu
